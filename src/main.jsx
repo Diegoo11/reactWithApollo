28580370c99952ecde88ebadf389741dde9a2c78
Diegoo11 <ynoacamino@gmail.com>
2023-06-21 21:05:17 -0500

    new projec: React + Apollo(GrapgQL)

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  ApolloClient, HttpLink, InMemoryCache, ApolloProvider,
+} from '@apollo/client';
+import App from './App';
+import './index.css';
+
+const client = new ApolloClient({
+  connectToDevTools: true,
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000/',
+  }),
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+);
