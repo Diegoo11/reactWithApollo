6765a333ffc13b5736f8c7295ff0baa71809b8d1
Diegoo11 <ynoacamino@gmail.com>
2023-06-27 21:10:35 -0500

    config for use apolloClient

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const client = new ApolloClient({
   connectToDevTools: true,
   cache: new InMemoryCache(),
   link: new HttpLink({
-    uri: 'http://localhost:4000/',
+    uri: 'http://localhost:5000/',
   }),
 });
 
